1b9acfb9d754f4ff2662d72544b35d5dadf5136a
Fernando <fermayo@gmail.com>
2013-10-30 12:53:11 -0300

    changed order of print statement

--- ssh-tunnel-daemon.sh
+++ ssh-tunnel-daemon.sh
@@ -9,6 +9,6 @@ source $1
 
 for ((i=0; i < ${#HOST[@]}; i++))
 do
-	echo "=> Tunneling ${REMOTE_USER[$i]}@${HOST[$i]}:${REMOTE_PORT[$i]} to localhost:${LOCAL_PORT[$i]}"
+	echo "=> Tunneling localhost:${LOCAL_PORT[$i]} to ${REMOTE_USER[$i]}@${HOST[$i]}:${REMOTE_PORT[$i]}"
 	autossh -M 0 -q -N -f -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -L ${LOCAL_PORT[$i]}:localhost:${REMOTE_PORT[$i]} ${REMOTE_USER[$i]}@${HOST[$i]}
 done
