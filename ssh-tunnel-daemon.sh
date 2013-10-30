de201313229cc9269c5a677c437d762e42bb4cca
Fernando <fermayo@gmail.com>
2013-10-30 13:30:25 -0300

    fix wait for jobs

--- ssh-tunnel-daemon.sh
+++ ssh-tunnel-daemon.sh
@@ -10,7 +10,7 @@ source $1
 for ((i=0; i < ${#HOST[@]}; i++))
 do
 	echo "=> Tunneling localhost:${LOCAL_PORT[$i]} to ${REMOTE_USER[$i]}@${HOST[$i]}:${REMOTE_PORT[$i]}"
-	autossh -M 0 -q -N -f -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -o "StrictHostKeyChecking no" -L ${LOCAL_PORT[$i]}:localhost:${REMOTE_PORT[$i]} ${REMOTE_USER[$i]}@${HOST[$i]}
+	autossh -M 0 -q -N -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -o "StrictHostKeyChecking no" -L ${LOCAL_PORT[$i]}:localhost:${REMOTE_PORT[$i]} ${REMOTE_USER[$i]}@${HOST[$i]} &
 done
 
 for job in `jobs -p`
